2e5237e3b22905789349f9ab31b332a63dea5248
Josh Cheek <josh.cheek@gmail.com>
2015-03-08 11:21:55 -0600

    Uses setTimeout, we want to get "after call" to the end

--- example.js
+++ example.js
@@ -7,9 +7,12 @@ var Sync = new Synchronize();
 
 var waitForTimeouts = function(timeouts, callback) {
   for(var i=0; i < timeouts.length; ++i) {
-    callback(timeouts[i]);
+    let timeout = timeouts[i]
+    setTimeout( ()=>callback(timeout), timeout)
   }
 }
 
 var timeoutTimes = [1, 4, 2, 3]
+console.log("before call");
 waitForTimeouts(timeoutTimes, seconds => console.log(`Waited ${seconds} seconds`))
+console.log("after call");
